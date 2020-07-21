b05ae6ec5042c95eaa92db4293b68f32e4309783
Fernand <fernando.vgarcia@hotmail.com>
2020-07-21 19:06:05 -0300

    Svg animation.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,57 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
+const svgVariants = {
+  initial: {
+    rotate: -180
+  },
+  animate: {
+    rotate: 0,
+    transition: {
+      duration: 1
+    }
+  }
+};
+
+const pathVariantes = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut"
+    }
+  }
+}
+
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
+        <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+          variants={svgVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.path
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariantes}
+            initial="hidden"
+            animate="visible"
           />
-          <path
+          <motion.path
             fill="none"
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVariantes}
+            initial="hidden"
+            animate="visible"
           />
-        </svg>
+        </motion.svg>
       </div>
       <motion.div className="title"
         initial={{ y: -250 }}
